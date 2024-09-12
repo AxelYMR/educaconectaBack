b6fdb8da1ad773010161f7d15217a15cb3e6660a
AxelYMR <axelyahir0209@gmail.com>
2024-09-12 10:43:27 -0600

    Añadi querys para verificar que un usuario se encuentre en la BD

---
+++ querys.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3001;
+
+const Pool = require('pg').Pool
+var fs = require('fs');
+
+app.use(cors());
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'educaconecta',
+    password: 'root',
+    port: 5432
+});
+
+app.use(express.json());
+
+app.post("/createU", (request, response) => {
+    const correo= request.body.correo;
+    const nombre = request.body.nombre;
+    const apellido = request.body.apellido;
+    const contrasenia = request.body.contrasenia;
+    pool.query('INSERT INTO alumno (correo,nombre,apellido, contrasenia) VALUES ($1, $2,$3, $4)', [correo,nombre,apellido,contrasenia], (error, results) => {
+        if (error) {
+            throw error
+        }else{
+            response.send(results);
+        }
+    })
+});
+
+app.get("/selectU", (request, response) => {
+    const correo= request.body.correo;
+    const contrasenia = request.body.contrasenia;
+    pool.query('Select correo, contrasenia from alumno WHERE correo = $1 AND contrasenia = $2',[correo, contrasenia], (error, result) => {
+        if (error) {
+            throw error
+        }else{
+            response.status(200).json(result.rows)
+        }
+    })
+});
+
+app.listen(port, () => {
+    console.log(`APP running on port ${port}.`)
+});

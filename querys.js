7e1e2b81f137791a2eee130d8d4a000d99180f6a
AxelYMR <axelyahir0209@gmail.com>
2024-12-03 12:41:16 -0600

    Actualizacion de los metodos de bd Gestionar Curso, Gestionar Contenido y Agregar Notificaciones

--- querys.js
+++ querys.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const multer = require('multer');
 const cors = require("cors");
 const port = 3001;
 
@@ -230,6 +231,259 @@ app.get("/materias", (request, response) => {
     });
 });
 
+app.get("/materia/:id", (request, response) => {
+    const id = request.params.id;
+    pool.query('SELECT * FROM materias WHERE id_materia = $1', [id], (error, results) => {
+        if (error) {
+            return response.status(500).json({ error: error.message });
+        } else {
+            return response.status(200).json(results.rows[0]); // Devuelve solo el primer resultado
+        }
+    });
+});
+
+app.get("/buscarMateriasA", (request, response) => { //seleccionar datos de la tabla docentes
+    const matricula= request.query.matricula;
+    pool.query('SELECT * FROM materias WHERE id_materia in(SELECT materias_id_materia from alumnos_has_materias where alumnos_matricula = $1 )',[matricula], (error, result) => {
+        if (error) {
+            throw error
+        }else{
+            response.status(200).json(result.rows);
+        }
+    })
+});
+
+app.get("/buscarMateriasD", (request, response) => { //seleccionar datos de la tabla docentes
+    const matricula= request.query.matricula;
+    pool.query('SELECT * FROM materias WHERE id_materia in(SELECT materias_id_materia from materias_has_docentes where docentes_matricula = $1 )',[matricula], (error, result) => {
+        if (error) {
+            throw error
+        }else{
+            response.status(200).json(result.rows);
+        }
+    })
+});
+
+app.get("/materiasDisponibles", (request, response) => {
+    const matricula= request.query.matricula;
+    pool.query('select * from materias where id_materia not in (select materias_id_materia from alumnos_has_materias where alumnos_matricula = $1)',[matricula], (error, result) => {
+        if (error) {
+            throw error
+        }else{
+            response.status(200).json(result.rows);
+        }
+    })
+});
+
+app.get("/materiasInscritas", (request, response) => {
+    const matricula= request.query.matricula;
+    pool.query('select * from materias where id_materia in (select materias_id_materia from alumnos_has_materias where alumnos_matricula = $1)',[matricula], (error, result) => {
+        if (error) {
+            throw error
+        }else{
+            response.status(200).json(result.rows);
+        }
+    })
+});
+
+app.get("/inscribirCurso", (request, response) => {
+    const matricula= request.query.matricula;
+    const id_materia= request.query.id_materia;
+    pool.query('insert into alumnos_has_materias(alumnos_matricula, materias_id_materia) values ($1, $2)',[matricula,id_materia], (error, result) => {
+        if (error) {
+            throw error
+        }else{
+            response.status(200).json(result.rows);
+        }
+    })
+});
+
+app.get("/darDeBaja", (request, response) => {
+    const matricula= request.query.matricula;
+    const id_materia= request.query.id_materia;
+    pool.query('delete from alumnos_has_materias where alumnos_matricula = $1 and materias_id_materia = $2',[matricula,id_materia], (error, result) => {
+        if (error) {
+            throw error
+        }else{
+            response.status(200).json(result.rows);
+        }
+    })
+});
+
+app.get("/cantidadDeCursos", (request, response) => {
+    const matricula= request.query.matricula;
+    pool.query('SELECT COUNT(*) AS count FROM alumnos_has_materias WHERE alumnos_matricula = $1',[matricula], (error, result) => {
+        if (error) {
+            throw error
+        }else{
+            response.status(200).json(result.rows);
+        }
+    })
+});
+
+app.get("/cantidadDeAlumnos", (request, response) => {
+    const id_materia= request.query.id_materia;
+    pool.query('SELECT COUNT(*) AS count FROM alumnos_has_materias WHERE materias_id_materia = $1',[id_materia], (error, result) => {
+        if (error) {
+            throw error
+        }else{
+            response.status(200).json(result.rows);
+        }
+    })
+});
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads');
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' + file.originalname);
+    }
+  });
+  const upload = multer({ storage: storage });
+
+  app.use('/uploads', express.static('uploads'));
+  
+  // Ruta para manejar la subida de archivos
+  app.post('/subirMultimedia', upload.single('file'), (req, res) => {
+    const { materiaId, tipo, descripcion } = req.body;
+    const filePath = req.file.path.replace(/\\/g, '/');;
+    // Aquí puedes guardar la dirección del archivo, el ID de la materia y el tipo en la base de datos
+    pool.query('INSERT INTO actividades (materias_id_materias,nombre,tipo, upload) VALUES ($1, $2, $3, $4)', [materiaId, descripcion, tipo, filePath], (error, results) => {
+        if (error) {
+            console.error('Error al insertar en la base de datos:', error);
+            return res.status(500).send('Error al insertar en la base de datos');
+        }else{
+            console.log('File path:', filePath);
+            console.log('Materia ID:', materiaId);
+            console.log('Tipo:', tipo);
+            console.log('Descripcion:', descripcion);
+            res.status(200).json({ filePath: filePath });
+        }
+    })
+  });
+
+app.get("/actividades", (request, response) => {
+    const id= request.query.id;
+    pool.query('SELECT * from actividades where materias_id_materias =$1 order by id_actividad desc',[id], (error, result) => {
+        if (error) {
+            throw error
+        }else{
+            response.status(200).json(result.rows);
+        }
+    })
+});
+
+app.delete("/eliminarActividad/:id1", (request, response) => {
+    const id1 = request.params.id1;
+    pool.query('delete from actividades where id_actividad = $1',[id1], (error, results) => {
+        if (error) {
+            throw error
+        }else{
+            response.send(results);
+        }
+    })
+});
+
+app.post('/crearExamen', (req, res) => {
+    const { materiaId, questions } = req.body;
+  
+    pool.query('INSERT INTO examenes (id_materia) VALUES ($1) RETURNING id_examen', [materiaId], (error, results) => {
+      if (error) {
+        console.error('Error al insertar en la base de datos:', error);
+        return res.status(500).send('Error al insertar en la base de datos');
+      }
+  
+      const examenId = results.rows[0].id_examen;
+  
+      const questionPromises = questions.map(q => {
+        return pool.query('INSERT INTO preguntas (id_examen, pregunta, respuesta) VALUES ($1, $2, $3)', [examenId, q.question, q.answer]);
+      });
+  
+      Promise.all(questionPromises)
+        .then(() => {
+          res.status(200).send('Examen creado exitosamente');
+        })
+        .catch(error => {
+          console.error('Error al insertar preguntas en la base de datos:', error);
+          res.status(500).send('Error al insertar preguntas en la base de datos');
+        });
+    });
+  });
+
+  app.get("/examenes", (request, response) => {
+    const id = request.query.id;
+    pool.query(`
+        SELECT e.id_examen, p.id_pregunta, p.pregunta, p.respuesta
+        FROM examenes e
+        JOIN preguntas p ON e.id_examen = p.id_examen
+        WHERE e.id_materia = $1
+        ORDER BY e.id_examen, p.id_pregunta
+    `, [id], (error, result) => {
+        if (error) {
+            console.error('Error al obtener los exámenes:', error);
+            response.status(500).send('Error al obtener los exámenes');
+        } else {
+            const examenes = {};
+            result.rows.forEach(row => {
+                if (!examenes[row.id_examen]) {
+                    examenes[row.id_examen] = {
+                        id_examen: row.id_examen,
+                        preguntas: []
+                    };
+                }
+                examenes[row.id_examen].preguntas.push({
+                    id_pregunta: row.id_pregunta,
+                    pregunta: row.pregunta,
+                    respuesta: row.respuesta
+                });
+            });
+            response.status(200).json(Object.values(examenes));
+        }
+    });
+});
+
+app.post('/crearNotificacion', (req, res) => {
+    const { id_materia, titulo, mensaje } = req.body;
+  
+    pool.query('INSERT INTO notificaciones (id_materia, titulo, mensaje) VALUES ($1, $2, $3) RETURNING *', [id_materia, titulo, mensaje], (error, results) => {
+      if (error) {
+        console.error('Error al insertar en la base de datos:', error);
+        return res.status(500).send('Error al insertar en la base de datos');
+      }
+      res.status(200).json(results.rows[0]);
+    });
+  });
+  
+  // Ruta para obtener las notificaciones de una materia
+  app.get('/notificaciones', (req, res) => {
+    const id_materia = req.query.id_materia;
+  
+    pool.query('SELECT * FROM notificaciones WHERE id_materia = $1 ORDER BY fecha DESC', [id_materia], (error, results) => {
+      if (error) {
+        console.error('Error al obtener las notificaciones:', error);
+        return res.status(500).send('Error al obtener las notificaciones');
+      }
+      res.status(200).json(results.rows);
+    });
+  });
+
+  app.get("/verificarHora", (request, response) => {
+    const { matricula, hora } = request.query;
+    pool.query(`
+        SELECT m.nombre, m.hora
+        FROM materias m
+        JOIN alumnos_has_materias ahm ON m.id_materia = ahm.materias_id_materia
+        WHERE ahm.alumnos_matricula = $1 AND m.hora = $2
+    `, [matricula, hora], (error, result) => {
+        if (error) {
+            console.error('Error al verificar la hora:', error);
+            response.status(500).send('Error al verificar la hora');
+        } else {
+            response.status(200).json(result.rows);
+        }
+    });
+});
 
 app.listen(port, () => { //puerto en el que se esta corriendo el servidor.
     console.log(`APP running on port ${port}.`) //mensaje de que el servidor esta corriendo
